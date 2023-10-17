e99f6ce63570236b147755e11c790fada0c46203
NicoColman <nicolman01@gmail.com>
2023-10-17 13:13:56 -0300

    Se agregaron TPs

---
+++ trabajo-practico-9/TP9_2/MiNotSoSimpleAppTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MiNotSoSimpleAppTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
